3182ead7f0d28e5e81bda0b33de59110d3b92774
Komiya <jcso19027@g.nihon-u.ac.jp>
2020-12-16 13:57:11 +0900

    会話

---
+++ Assets/UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左クリックしたとき、オブジェクト表示
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(true);
+        }
+
+
+    }
+}
