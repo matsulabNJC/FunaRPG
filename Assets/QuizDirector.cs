70d5103f95febf7865cb6ee36e22545c5655be8f
Takahasi Keisuke <jcke19040@g.nihon-u.ac.jp>
2020-12-23 15:07:04 +0900

    シーン切替

---
+++ Assets/QuizDirector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // LoadSceneを使うために必要!!
+
+public class QuizDirector : MonoBehaviour
+{
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("あああああ");
+        if (collider.gameObject.tag == "Message")
+        {
+            SceneManager.LoadScene("Quiz");
+        }
+    }
+}
