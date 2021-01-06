1493a1c65c94e54e93401c1f6e00f1dfb52b6a32
WakiDaichi <jcdi19075@g.nihon-u.ac.jp>
2021-01-06 14:18:23 +0900

    スタート画面から遷移

---
+++ Assets/ButtonScript1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
