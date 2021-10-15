9434988dd89c813722a7e7d6b6ad509b607d50de
matsumei <86765503+matsumei@users.noreply.github.com>
2021-10-15 14:57:03 +0900

    入口ミニゲーム途中

---
+++ Assets/CardController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CardController : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x + moveX, -9.55f, 9.55f),
+            Mathf.Clamp(transform.position.y + moveY, -3.9f, 3.9f)
+            );
+    }
+    void OnMouseDown()
+    {
+        this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+        transform.position = currentPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ƒ^ƒbƒ`");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
