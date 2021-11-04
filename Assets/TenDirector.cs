f0bbe04f21f27880782b5ffab7b42225125f43ff
oguma0011 <86765512+oguma0011@users.noreply.github.com>
2021-11-04 16:49:23 +0900

    クイズ画面を作った。

--- Assets/TenDirector.cs
+++ Assets/TenDirector.cs
@@ -1,22 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // LoadSceneを使うために必要!!
 
-public class TenDirector : MonoBehaviour
+public class NewBaseType
 {
-    
-    
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-    }
-        void OnCollisionEnter2D(Collision2D collision)
-        {
         Debug.Log("あああああ");
-            if (collision.gameObject.tag == "Enter")
-            {
+        if (collision.gameObject.tag == "Enter")
+        {
             SceneManager.LoadScene("SampleScene");
-            }
+        }
         if (collision.gameObject.tag == "Enter4")
         {
             SceneManager.LoadScene("SampleScene");
@@ -28,6 +23,10 @@ public class TenDirector : MonoBehaviour
         if (collision.gameObject.tag == "Enter6")
         {
             SceneManager.LoadScene("Syokudou");
+            if (collision.gameObject.tag == "Ticket")
+            {
+                SceneManager.LoadScene("Ticket");
+            }
         }
     }
 }
